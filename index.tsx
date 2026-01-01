c4ddb798d09bcdce0168779560cb0594b723d55f
geovanesilvatst0-hue <geovanesilvatst0@gmail.com>
2026-01-01 18:27:21 -0300

    Fix error message quotes in index.tsx

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
@@ -13,5 +13,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
 );
