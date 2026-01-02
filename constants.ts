5d3d477920edd2e494a8832e668919dbbb8690ac
geovanesilvatst0-hue <geovanesilvatst0@gmail.com>
2026-01-02 13:38:54 -0300

    teste direto da fonte

--- constants.ts
+++ constants.ts
@@ -2,17 +2,17 @@
 import { Prize, StoreConfig } from './types';
 
 export const INITIAL_PRIZES: Prize[] = [
-  { id: '1', name: '10% de Desconto', description: 'Ganhou 10% na próxima compra!', isWinning: true },
+  { id: '1', name: '50% de Desconto', description: 'Ganhou 10% na próxima compra!', isWinning: true },
   { id: '2', name: 'Brinde Surpresa', description: 'Retire um brinde no balcão!', isWinning: true },
   { id: '3', name: 'Vale R$ 20,00', description: 'Desconto direto no caixa.', isWinning: true },
-  { id: '4', name: 'Tente Novamente', description: 'Não foi dessa vez!', isWinning: false },
+  { id: '4', name: 'Teste direto na fonte', description: 'Não foi dessa vez!', isWinning: false },
 ];
 
 export const INITIAL_CONFIG: StoreConfig = {
-  name: 'Minha Loja Premiada',
+  name: 'JG Presentes',
   logoUrl: 'https://cdn-icons-png.flaticon.com/512/606/606547.png',
   primaryColor: '#4f46e5',
-  whatsappNumber: '5511999999999',
+  whatsappNumber: '5564993408657',
   adminPassword: 'admin',
   // Número que receberá os leads do botão "Quero fazer minha própria raspadinha"
   adminContactNumber: '5564993408657',
